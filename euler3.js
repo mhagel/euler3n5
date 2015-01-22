3908ea569279814ff61f6511ceb33d70471f995c
mhagel <mark.hagelberg@gmail.com>
2015-01-22 09:52:39 -0700

    with explanation of process

--- euler3.js
+++ euler3.js
@@ -4,6 +4,8 @@
 
 // just solve for numbers smaller than 1,000 and don't worry about making it efficient. 
 
+//Step 1: Put prime factors (under 1,000) into an array:
+
 var primeFactors = [];
 
 function primeID(num) {
@@ -17,9 +19,11 @@ function primeID(num) {
 primeID(600851475143);
 
 
+
+//Step 2: Identify the largest value in the array and print the results:
+
 function maxOfArray(array) {
 	console.log(Math.max.apply(null, array));
 }
-
 
 maxOfArray(primeFactors);
