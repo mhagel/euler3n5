86b77ba6a8716b9edd558af04e3282d3876fb48c
mhagel <mark.hagelberg@gmail.com>
2015-01-22 09:45:25 -0700

    simplified

--- euler3.js
+++ euler3.js
@@ -4,12 +4,6 @@
 
 // just solve for numbers smaller than 1,000 and don't worry about making it efficient. 
 
-//wikipedia: For a prime factor p of n, the multiplicity of p is the largest exponent a for which pa divides n exactly.
-
-//so we need a way to identify prime factors by plugging in a number.
-
-// Maybe a function that checks for prime factors using modulo? It would take in a number as an argument and return all the prime factors in an array?
-
 var primeFactors = [];
 
 function primeID(num) {
@@ -22,10 +16,6 @@ function primeID(num) {
 
 primeID(600851475143);
 
-
-
-//OK, now need to identify the largest number in that array. Will probably search for an array method that does that.
-//mdn: The Math.max() function returns the largest of zero or more numbers.
 
 function maxOfArray(array) {
 	console.log(Math.max.apply(null, array));
